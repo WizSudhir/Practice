e4b2a1451918bc2f3c264fb382cab6c57219112c
WizSudhir <wizsudhir@gmail.com>
2026-03-12 09:49:18 +0530

    Update who-we-serve.js

--- js/who-we-serve.js
+++ js/who-we-serve.js
@@ -15,3 +15,30 @@ entry.target.classList.add("show");
 cards.forEach(card=>{
 observer.observe(card);
 });
+
+//* Filter JS *//
+const filterBtns = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".specialty-card");
+
+filterBtns.forEach(btn=>{
+
+btn.addEventListener("click",()=>{
+
+filterBtns.forEach(b=>b.classList.remove("active"));
+btn.classList.add("active");
+
+const filter = btn.dataset.filter;
+
+cards.forEach(card=>{
+
+if(filter==="all" || card.dataset.category===filter){
+card.style.display="block";
+}else{
+card.style.display="none";
+}
+
+});
+
+});
+
+});
