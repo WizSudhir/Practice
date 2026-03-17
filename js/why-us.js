4a459f2ffbb71adcc79f33808dd0d6e6fb60d03d
WizSudhir <wizsudhir@gmail.com>
2026-03-17 13:32:03 +0530

    Update why-us.js

--- js/why-us.js
+++ js/why-us.js
@@ -505,8 +505,10 @@ valueEl.innerText = "0%"
 }
 
 })
-},{ threshold:0.1 })
-rootMargin: "0px 0px -50px 0px"
+},{
+  threshold:0.1,
+  rootMargin: "0px 0px -50px 0px"
+})
 roiCards.forEach(card=>{
 roiObserver.observe(card)
 })
