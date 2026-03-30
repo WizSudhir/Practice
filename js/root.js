c84b51ea69dd7761639dbc6128d4217afad4e816
WizSudhir <wizsudhir@gmail.com>
2026-03-31 01:30:06 +0530

    Update root.js

--- js/root.js
+++ js/root.js
@@ -12,24 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(section);
   });
   // ===============================
-  // LIGHT/DARK THEME
-  // ===============================  
-  window.toggleTheme = () => {
-  const current = document.documentElement.getAttribute("data-theme");
-  if (current === "light") {
-    document.documentElement.removeAttribute("data-theme");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  }
-};
-// Load saved theme
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme === "light") {
-  document.documentElement.setAttribute("data-theme", "light");
-}
-  // ===============================
   // ELEMENTS
   // ===============================
   const navToggle = document.querySelector(".nav-toggle");
